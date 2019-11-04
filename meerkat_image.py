49cc07e70ce08013ddca90447358e354933daf89
Mziyanda Mngqibisa <mziyandamngqibisa@yahoo.com>
2019-11-04 11:21:41 +0200

    Changed imaging pipeline and removed applycal

--- meerkat_image.py
+++ meerkat_image.py
@@ -3,13 +3,8 @@ field_1='/data/mziyanda/sarao/field_1/'
 field_2='/data/mziyanda/sarao/field_2/'
 target='/data/mziyanda/sarao/target/'
 msfile='el_gordo'
-'''
-applycal(vis=msfile+'.ms',field='3',
-        gaintable=[field_0+msfile+'.B0',
-        field_0+msfile+'.fluxscale1',
-        field_0+msfile+'.K0'],
-        gainfield=['','J0024-4202',''],interp=['','linear',''],
-        parang=False,calwt=False,applymode='calflag')
+
+
 applycal(vis=msfile+'.ms',field='3',
         gaintable=[field_1+msfile+'.B0',
         field_1+msfile+'.fluxscale1',
@@ -17,11 +12,10 @@ applycal(vis=msfile+'.ms',field='3',
         gainfield=['','J0024-4202',''],interp=['','linear',''],
         parang=False,calwt=False,applymode='calflag')
 
-'''
 tclean(vis=msfile+'.ms',field='3',specmode='mfs',
 	deconvolver='mtmfs',threshold='0mJy',
 	gridder='wproject',imsize=[3888],nterms=2,
-	cell=['4arcsec'],wprojplanes=-1,width=300,
+	cell=['4arcsec'],wprojplanes=-1,width=200,
 	weighting='briggs',niter=0,interactive=False,
 	imagename=target+'ElGordo-I-clean-mask-full',
 	robust=0.8,pblimit=-0.01,pbcor=False,
@@ -30,7 +24,6 @@ tclean(vis=msfile+'.ms',field='3',specmode='mfs',
 	minbeamfrac=0.3,growiterations=75,
 	negativethreshold=15.0,verbose=True)
 
-'''
 tclean(vis=msfile+'.ms',field='3',scan='4',antenna='m003',
 	specmode='mfs',deconvolver='mtmfs',gridder='wproject',
 	imsize=[2700],cell=['4arcsec'],weighting='briggs',
@@ -46,7 +39,3 @@ tclean(vis=msfile+'.ms',field='3',scan='4',antenna='m003',
         usemask='auto-multithresh',sidelobethreshold=2.0,noisethreshold=4.25,
         lownoisethreshold=1.5,minbeamfrac=0.3,growiterations=75,
         negativethreshold=15.0,verbose=True)
-
-
-
-'''
